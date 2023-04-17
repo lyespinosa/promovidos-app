186310436ca91e54f3bd8dcd41a140d662bad6e0
DiegoAlbertoPerezJuarez <213475@ids.upchiapas.edu.mx>
2023-04-17 09:15:02 -0600

    post

--- screens/Insert.js
+++ screens/Insert.js
@@ -4,12 +4,138 @@ import { useNavigation } from "@react-navigation/native";
 import * as ImagePicker from "expo-image-picker";
 import { SelectList } from "react-native-dropdown-select-list";
 import DefaultStyles from "../styles/DefaultStyles";
+import axios from 'axios';
 
 const Insert = () => {
     const [image, setImage] = useState(null);
-    const [cateogry, setCateogry] = useState("")
+    const [cateogry, setCateogry]= useState("")
+    const [nombre, setNombre] = useState("")
+    const [paterno, setPaterno] = useState("")
+    const [materno, setMaterno] = useState("")
+    const [cp, setCp] = useState("")
+    const [folio,setFolio] = useState("")
+    const [curp,setCurp] = useState("")
+    const [ine,setIne] = useState("")
+    const [cargo,setCargo] = useState("")
+    const [ocupacion,setOcupacion] = useState("")
+    const [estructuras,setEstructura] = useState("")
+    const [estado,setEstado] = useState("")
+    const [municipios,setMunicipio] = useState("")
+    const [localidad,setLocalidad] = useState("")
+    const [colonia,setColonia] = useState("")
+    const [direccion,setDireccion] = useState("")
+    const [exterior,setExterior] = useState("")
+    const [celular,setCelular] = useState("")
+    const [correo,setCorreo] = useState("")
+    const [seccion,setSeccion] = useState("")
+    const [sexo,setSexo] = useState("")
     const [subCategory, setSubCategory] = useState("")
-
+      const [response, setResponse] = useState("");
+    
+      const handleFolioChange = (folio) => {
+        console.log(folio)
+        setFolio(folio)
+      };
+      const handleIneChange = (ine) => {
+        setIne(ine);
+      };
+    
+      const handleSeccionChange = (seccion) => {
+        setSeccion(seccion);
+      };
+      const handleEstrucuturaChange = (estructura) => {
+        console.log(estructura)
+        setEstructura(estructura);
+      };
+      const handleCargoChange = (cargo) => {
+        setCargo(cargo);
+      };
+      const handleOcupacionChange = (ocupacion) => {
+        setOcupacion(ocupacion);
+      };
+      const handleNombreChange = (nombre) => {
+        setNombre(nombre);
+      };
+      const handlePaternoChange = (paterno) => {
+        setPaterno(paterno);
+      };
+      const handleMaternoChange = (materno) => {
+        setMaterno(materno);
+      };
+    
+      const handleSexoChange = (sexo) => {
+        setSexo(sexo);
+      };
+      const handleCurpChange = (curp) => {
+        setCurp(curp);
+      };
+      const handleEstadoChange = (estado) => {
+        setEstado(estado);
+      };
+      const handleMunicipioChange = (municipio) => {
+        setMunicipio(municipio);
+      };
+      const handleLocalidadChange = (localidad) => {
+        setLocalidad(localidad);
+      };
+      const handleColoniaChange = (colonia) => {
+        setColonia(colonia);
+      };
+      const handleDireccionChange = (direccion) => {
+        setDireccion(direccion);
+      };
+      const handleExteriorChange = (exterior) => {
+        setExterior(exterior);
+      };
+      const handleCpChange = (cp) => {
+        setCp(cp);
+      };
+      const handleCelularChange = (celular) => {
+        setCelular(celular);
+      };
+      const handleCorreoChange = (correo) => {
+        setCorreo(correo);
+        console.log(correo)
+      };
+      const handleSubmit = () => {
+        fetch('http://192.168.1.131:8000/api/promotors/create', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+        "folio": folio,
+        "ine": ine,
+        "seccion": 1,
+        "estructura": 1,
+        "cargo": cargo,
+        "ocupacion": ocupacion,
+        "nombre": nombre,
+        "paterno": paterno,
+        "materno": materno,
+        "sexo": 1,
+        "curp": curp,
+        "estado": 1,
+        "municipio": 2,
+        "localidad": 1,
+        "colonia": colonia,
+        "direccion": direccion,
+        "exterior": exterior,
+        "cp": cp,
+        "celular": celular,
+        "correo": correo,
+        "activo": 1,
+        "usuario": 1
+  })
+})
+.then(response => response.json())
+.then(data => {
+  console.log(data);
+})
+.catch(error => {
+  console.error(error);
+});
+      };
 
 
     const estructura = [
@@ -28,8 +154,8 @@ const Insert = () => {
     ];
 
     const sexoList = [
-        { key: 'H', value: 'Masculino' },
-        { key: 'F', value: 'Femenino' },
+        { key: '1', value: 'Masculino' },
+        { key: '2', value: 'Femenino' },
     ];
 
     const [isEnabled, setIsEnabled] = useState(false);
@@ -94,16 +220,22 @@ const Insert = () => {
                             style={[DefaultStyles.input]}
                             className="w-full border-[#686868] border-[1px]"
                             placeholder="Nombre"
+                            onChangeText={handleNombreChange}
+                            
                         />
                         <TextInput
                             style={[DefaultStyles.input, DefaultStyles.mt_8]}
                             className="w-full border-[#686868] border-[1px]"
                             placeholder="Apellido paterno"
+                            onChangeText={handlePaternoChange}
+                            
                         />
                         <TextInput
                             style={[DefaultStyles.input, DefaultStyles.mt_8]}
                             className="w-full border-[#686868] border-[1px]"
                             placeholder="Apellido materno"
+                            onChangeText={handleMaternoChange}
+                            
                         />
                         <View style={[ DefaultStyles.mt_8]} >
                             <SelectList
@@ -113,6 +245,8 @@ const Insert = () => {
                                 boxStyles={{ backgroundColor: 'rgb(248,250,252)' }}
                                 dropdownStyles={{ backgroundColor: 'rgb(248,250,252)' }}
                                 placeholder="Sexo"
+                                
+                                
                             />
                         </View>
 
@@ -125,68 +259,85 @@ const Insert = () => {
                         style={[DefaultStyles.input, DefaultStyles.mt_8]}
                         className="w-full border-[#686868] border-[1px]"
                         placeholder="Folio"
+                        
+                        onChangeText={handleFolioChange}
                     />
                     <TextInput
                         style={[DefaultStyles.input, DefaultStyles.mt_8]}
                         className="w-full border-[#686868] border-[1px]"
                         placeholder="INE o Clave de elector"
+                        onChangeText={handleIneChange}
+                        
                     />
                     <TextInput
                         style={[DefaultStyles.input, DefaultStyles.mt_8]}
                         className="w-full border-[#686868] border-[1px]"
                         placeholder="Seccion"
+                        onChangeText={handleSeccionChange}
+                        
                     />
                     <TextInput
                         style={[DefaultStyles.input, DefaultStyles.mt_8]}
                         className="w-full border-[#686868] border-[1px]"
                         placeholder="CURP"
                         maxLength={18}
+                        onChangeText={handleCurpChange}
+                        
                     />
                     <View style={[DefaultStyles.mt_8]}>
                         <SelectList
                             data={estructura}
                             search={false}
-                            setSelected={setCateogry}
+                            setSelected={handleEstrucuturaChange}
                             boxStyles={{ backgroundColor: 'rgb(248,250,252)' }}
                             dropdownStyles={{ backgroundColor: 'rgb(248,250,252)' }}
                             placeholder="Estructura"
+                            
                         />
                     </View>
                     <TextInput
                         style={[DefaultStyles.input, DefaultStyles.mt_8]}
                         className="w-full border-[#686868] border-[1px]"
                         placeholder="Cargo"
-
+                        onChangeText={handleCargoChange}
+                        
                     />
                     <TextInput
                         style={[DefaultStyles.input, DefaultStyles.mt_8]}
                         className="w-full border-[#686868] border-[1px]"
                         placeholder="Ocupacion"
+                        onChangeText={handleOcupacionChange}
                     />
                     <View style={[DefaultStyles.mt_8]}>
                         <SelectList
                             data={municipio}
                             search={false}
-                            setSelected={setCateogry}
+                            setSelected={handleMunicipioChange}
                             boxStyles={{ backgroundColor: 'rgb(248,250,252)' }}
                             dropdownStyles={{ backgroundColor: 'rgb(248,250,252)' }}
                             placeholder="Municipio"
+                            
+                            
                         />
                     </View>
                     <View style={[DefaultStyles.mt_8]}>
                         <SelectList
                             data={localidades}
                             search={false}
-                            setSelected={setCateogry}
+                            setSelected={handleLocalidadChange}
                             boxStyles={{ backgroundColor: 'rgb(248,250,252)' }}
                             dropdownStyles={{ backgroundColor: 'rgb(248,250,252)' }}
                             placeholder="Localidades"
+                            
+                            
                         />
                     </View>
                     <TextInput
                         style={[DefaultStyles.input, DefaultStyles.mt_8]}
                         className="w-full border-[#686868] border-[1px]"
                         placeholder="Colonia"
+                        onChangeText={handleColoniaChange}
+                        
                     />
 
                     <TextInput
@@ -195,30 +346,40 @@ const Insert = () => {
                         placeholder="Dirección"
                         multiline={true}
                         numberOfLines={3}
+                        onChangeText={handleDireccionChange}
+                        
                     />
 
                     <TextInput
                         style={[DefaultStyles.input, DefaultStyles.mt_8]}
                         className="w-full border-[#686868] border-[1px]"
-                        placeholder="Número"
+                        placeholder="Número exterior"
                         keyboardType="numeric"
+                        onChangeText={handleExteriorChange}
+                        
                     />
                     <TextInput
                         style={[DefaultStyles.input, DefaultStyles.mt_8]}
                         className="w-full border-[#686868] border-[1px]"
                         placeholder="Código postal"
                         keyboardType="numeric"
+                        onChangeText={handleCpChange}
+                        
                     />
                     <TextInput
                         style={[DefaultStyles.input, DefaultStyles.mt_8]}
                         className="w-full border-[#686868] border-[1px]"
                         placeholder="Número celular"
                         keyboardType="numeric"
+                        onChangeText={handleCelularChange}
+                        
                     />
                     <TextInput
                         style={[DefaultStyles.input, DefaultStyles.mt_8]}
                         className="w-full border-[#686868] border-[1px]"
                         placeholder="Correo electrónico"
+                        onChangeText={handleCorreoChange}
+                       
                     />
                     <View
                         style={DefaultStyles.mt_8}
@@ -236,7 +397,7 @@ const Insert = () => {
 
                     <TouchableOpacity
                         style={DefaultStyles.mt_8}
-                        onPress={""} //hacer el POST ahí
+                        onPress={handleSubmit} //hacer el POST ahí
                         className="bg-blue-500 m-auto px-14 py-4 rounded-md"
                     >
                         <Text className="font-semibold text-[24px] text-white">Agregar</Text>
