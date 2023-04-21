b605b2c7ba379b517dcdc6ce627e9aa5a958f84a
JaviXDLOL16 <100722991+JaviXDLOL16@users.noreply.github.com>
2023-04-21 13:32:27 -0600

    fix style

--- components/MyModal.js
+++ components/MyModal.js
@@ -1,4 +1,4 @@
-import { View, Text, Modal, Button } from "react-native";
+import { View, Text, Modal, Button, TouchableOpacity } from "react-native";
 import React from "react";
 import { FontAwesome } from '@expo/vector-icons';
 import { BlurView } from "expo-blur";
@@ -16,10 +16,11 @@ const MyModal = ({ isModalOpen, setIsModalOpen }) => {
       <View className="flex-1 justify-center bg-gray-300/[0.4] blur-2xl">
         <BlurView intensity={5}>
           <View className="items-center h-[80vh] my-[20vh]  bg-white mx-5 border-2 border-[#E8E8E8] rounded-3xl overflow-hidden shadow-md shadow-[#E8E8E8]">
-            <View className="flex-row border-b border-[#E8E8E8] bg-red-500 w-full p-2">
-            <Text className="flex-1 text-center pl-6 text-white font-extrabold text-[20px]">Promovidos</Text>
-              <FontAwesome name="close" size={24} color="white" onPress={() => setIsModalOpen(!setIsModalOpen)}/>
-            </View>
+            
+            <TouchableOpacity activeOpacity={1}   onPress={() => setIsModalOpen(!setIsModalOpen)} className="flex-row border-b border-[#E8E8E8] bg-red-500 w-full p-2">
+            <Text className="flex-1 text-center pl-6 text-white font-extrabold text-[20px]">Promovido</Text>
+              <FontAwesome name="close" size={24} color="white"/>
+            </TouchableOpacity>
           </View>
         </BlurView>
       </View>
